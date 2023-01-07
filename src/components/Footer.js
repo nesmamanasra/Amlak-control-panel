4f272d7d957eed4844e73e99ca4c914774ddc3f0
nesmamanasra <nesmazak1999@gmail.com>
2023-01-07 15:02:11 +0200

    add files component header menu dashboard footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+    render() {
+        return (
+           <div>
+  <footer className="main-footer">
+    <strong>Copyright © 2023 Aqar.ps. All rights reserved.</strong>
+  </footer>
+</div>
+
+        )
+    }
+}
